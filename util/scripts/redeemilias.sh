0fcb82f2344b606506e0c36556076c672fafe09a
Adolfo Neto <adolfo.usp@gmail.com>
2025-10-17 15:25:29 -0300

    python script to merge

--- util/scripts/redeemilias.sh
+++ util/scripts/redeemilias.sh
@@ -4,4 +4,13 @@
 # ./redeemilias.sh 
 
 
+#💡 Dica: toda vez que abrir o terminal, ative o venv com:
+
+#source venv/bin/activate
+
+
+# Para sair do ambiente virtual, rode:
+
+# deactivate
+
 python merge_rss.py https://anchor.fm/s/820ceb8/podcast/rss https://anchor.fm/s/10f2ba74/podcast/rss https://anchor.fm/s/248c0568/podcast/rss https://anchor.fm/s/4d4944e8/podcast/rss https://anchor.fm/s/81acde0c/podcast/rss
