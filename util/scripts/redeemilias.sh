a95bd2a98f6e4ebaa93b8cb75025604cd101622e
Adolfo Neto <adolfo.usp@gmail.com>
2025-10-27 08:23:18 -0300

    nova rede

--- util/scripts/redeemilias.sh
+++ util/scripts/redeemilias.sh
@@ -1,16 +1,18 @@
+#!/bin/bash
+
 # python3 -m venv venv
 # source venv/bin/activate
 # pip install feedparser
 # ./redeemilias.sh 
 
 
-#💡 Dica: toda vez que abrir o terminal, ative o venv com:
+#💡 Dica: toda vez que abrir o terminal, ative o venv
+# source  venv/bin/activate
+# deactivate with deactivate
 
-#source venv/bin/activate
 
 
-# Para sair do ambiente virtual, rode:
+cd "/mnt/windows/__LINUX/home/adolfo/Desktop/Github Relevante/fronteirases.github.io/util/scripts"
+"/mnt/windows/__LINUX/home/adolfo/Desktop/Github Relevante/fronteirases.github.io/util/scripts/venv/bin/python" merge_rss.py https://anchor.fm/s/820ceb8/podcast/rss https://anchor.fm/s/10f2ba74/podcast/rss https://anchor.fm/s/248c0568/podcast/rss https://anchor.fm/s/4d4944e8/podcast/rss https://anchor.fm/s/81acde0c/podcast/rss
 
-# deactivate
 
-python merge_rss.py https://anchor.fm/s/820ceb8/podcast/rss https://anchor.fm/s/10f2ba74/podcast/rss https://anchor.fm/s/248c0568/podcast/rss https://anchor.fm/s/4d4944e8/podcast/rss https://anchor.fm/s/81acde0c/podcast/rss
